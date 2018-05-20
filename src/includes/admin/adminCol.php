1084feb017a5ca42d090b47d63e6ced21f9b4fa6
John Leschinski <picard102@gmail.com>
2018-05-20 10:47:44 -0400

    Admin Columns for Social

--- src/includes/admin/adminCol.php
+++ src/includes/admin/adminCol.php
@@ -64,3 +64,44 @@ switch ( $column ) {
 
 } //$column
 }
+
+
+
+
+
+
+
+
+
+
+/**
+* Admin Column Addtions
+*/
+function set_edit_social_columns( $columns ) {
+unset( $columns[ 'date' ] );
+unset( $columns[ 'author' ] );
+return array_merge( $columns, array(
+'images' => __( 'Image' ),
+'service' => __( 'Service' )
+) );
+}
+
+add_filter( 'manage_edit-social_columns', 'set_edit_social_columns' );
+add_action( 'manage_posts_custom_column', 'custom_social_columns' );
+
+function custom_social_columns( $column ) {
+  global $post;
+  switch ( $column ) {
+    case 'images':
+      $custom_image =  isset( get_post_meta( $post->ID, 'media_image', false)[0]) ? get_post_meta( $post->ID, 'media_image') : false;
+      if ($custom_image) {
+        $custom_image = wp_get_attachment_image_src( $custom_image[0], 'thumbnail')[0];
+        echo '<img src="'.$custom_image.'"/>';
+      }
+    break;
+    case 'service':
+      $active = isset( get_post_meta( $post->ID , 'type')[0] ) ?  get_post_meta( $post->ID , 'type')[0] : false ;
+       include( get_stylesheet_directory() . '/assets/svg/icons/icon-'.$active.'.svg' );
+    break;
+  }
+}
